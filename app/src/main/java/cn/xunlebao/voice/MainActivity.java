b25fbc91894db4dd55e1e3a1682b1a5cff3a3d48
Vincent <Vincent_0728@outlook.com>
2017-07-19 23:51:45 +0800

    优化

--- app/src/main/java/cn/xunlebao/voice/MainActivity.java
+++ app/src/main/java/cn/xunlebao/voice/MainActivity.java
@@ -9,17 +9,21 @@ import android.graphics.Color;
 import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.RelativeLayout;
 import android.widget.SeekBar;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.airsaid.pickerviewlibrary.OptionsPickerView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Random;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -38,25 +42,36 @@ public class MainActivity extends AppCompatActivity {
     private AudioManager mAudioManger;
     private MyVolumeReceiver mReceiver;
     private OptionsPickerView mMusicPik;
+    private OptionsPickerView mTimePicker;
     public static boolean isStart = false;
+    private Thread mThread;
 
     private String[] mMusicName = {
-        "雨声-1", "雨声-2", "雨声-3", "雨声-4", "雨声-5", "雨声-6", "雨声-7", "雨声-8", "雨声-9", "雨声-10", "雨声-11", "雨声-12", "雨声-13", "雨声-14", "雨声-15", "雨声-16"
-        , "火-1", "火-2", "火-3", "火-4", "火-5", "火-6"
-        , "森林-1", "森林-2", "森林-3", "森林-4", "森林-5", "森林-6", "森林-7", "森林-8", "森林-9", "森林-10", "森林-11", "森林-12", "森林-13"
-        , "海-1", "海-2", "海-3", "海-4", "海-5", "海-6"
-        , "风-1", "风-2", "风-3", "风-4", "风-5", "风-6", "风-7"
-        , "冥想-1", "冥想-2", "冥想-3", "冥想-4", "冥想-5"
+            "雨声-1", "雨声-2", "雨声-3", "雨声-4", "雨声-5", "雨声-6", "雨声-7", "雨声-8", "雨声-9", "雨声-10", "雨声-11", "雨声-12", "雨声-13", "雨声-14", "雨声-15", "雨声-16"
+            , "火-1", "火-2", "火-3", "火-4", "火-5", "火-6"
+            , "森林-1", "森林-2", "森林-3", "森林-4", "森林-5", "森林-6", "森林-7", "森林-8", "森林-9", "森林-10", "森林-11", "森林-12", "森林-13"
+            , "海-1", "海-2", "海-3", "海-4", "海-5"
+            , "风-1", "风-2", "风-3", "风-4", "风-5", "风-6", "风-7"
+            , "冥想-1", "冥想-2", "冥想-3", "冥想-4", "冥想-5"
     };
     private Integer[] mMusicRec = {
-        R.raw.rain_1, R.raw.rain_2, R.raw.rain_3, R.raw.rain_4, R.raw.rain_5, R.raw.rain_6, R.raw.rain_7, R.raw.rain_8, R.raw.rain_9, R.raw.rain_10, R.raw.rain_11, R.raw.rain_12, R.raw.rain_13, R.raw.rain_14, R.raw.rain_15, R.raw.rain_16
-        , R.raw.fire_1, R.raw.fire_2, R.raw.fire_3, R.raw.fire_4, R.raw.fire_5, R.raw.fire_6
-        , R.raw.forest_1, R.raw.forest_2, R.raw.forest_3, R.raw.forest_4, R.raw.forest_5, R.raw.forest_7, R.raw.forest_8, R.raw.forest_9, R.raw.forest_10, R.raw.forest_11, R.raw.forest_12, R.raw.forest_13
-        , R.raw.sea_1, R.raw.sea_2, R.raw.sea_3, R.raw.sea_4, R.raw.sea_5
-        , R.raw.wind_1, R.raw.wind_2, R.raw.wind_3, R.raw.wind_4, R.raw.wind_5, R.raw.wind_6, R.raw.wind_7
-        , R.raw.meditation_1, R.raw.meditation_2, R.raw.meditation_3, R.raw.meditation_4, R.raw.meditation_5
+            R.raw.rain_1, R.raw.rain_2, R.raw.rain_3, R.raw.rain_4, R.raw.rain_5, R.raw.rain_6, R.raw.rain_7, R.raw.rain_8, R.raw.rain_9, R.raw.rain_10, R.raw.rain_11, R.raw.rain_12, R.raw.rain_13, R.raw.rain_14, R.raw.rain_15, R.raw.rain_16
+            , R.raw.fire_1, R.raw.fire_2, R.raw.fire_3, R.raw.fire_4, R.raw.fire_5, R.raw.fire_6
+            , R.raw.forest_1, R.raw.forest_2, R.raw.forest_3, R.raw.forest_4, R.raw.forest_5, R.raw.forest_6, R.raw.forest_7, R.raw.forest_8, R.raw.forest_9, R.raw.forest_10, R.raw.forest_11, R.raw.forest_12, R.raw.forest_13
+            , R.raw.sea_1, R.raw.sea_2, R.raw.sea_3, R.raw.sea_4, R.raw.sea_5
+            , R.raw.wind_1, R.raw.wind_2, R.raw.wind_3, R.raw.wind_4, R.raw.wind_5, R.raw.wind_6, R.raw.wind_7
+            , R.raw.meditation_1, R.raw.meditation_2, R.raw.meditation_3, R.raw.meditation_4, R.raw.meditation_5
     };
+    private Integer[] mRain = {R.drawable.rain_0, R.drawable.rain_1, R.drawable.rain_2, R.drawable.rain_3, R.drawable.rain_4, R.drawable.rain_5, R.drawable.rain_6, R.drawable.rain_7, R.drawable.rain_8, R.drawable.rain_9,};
+    private Integer[] mFire = {R.drawable.fire_0, R.drawable.fire_1, R.drawable.fire_2, R.drawable.fire_3, R.drawable.fire_4, R.drawable.fire_5, R.drawable.fire_6, R.drawable.fire_7, R.drawable.fire_8, R.drawable.fire_9};
+    private Integer[] mSea = {R.drawable.sea_0, R.drawable.sea_1, R.drawable.sea_2, R.drawable.sea_3, R.drawable.sea_4, R.drawable.sea_5, R.drawable.sea_6, R.drawable.sea_7, R.drawable.sea_8, R.drawable.sea_9};
+    private Integer[] mForest = {R.drawable.forest_0, R.drawable.forest_1, R.drawable.forest_2, R.drawable.forest_3, R.drawable.forest_4, R.drawable.forest_5, R.drawable.forest_6, R.drawable.forest_7, R.drawable.forest_8, R.drawable.forest_9,};
+    private Integer[] mWind = {R.drawable.wind_0, R.drawable.wind_1, R.drawable.wind_2, R.drawable.wind_3, R.drawable.wind_4, R.drawable.wind_5, R.drawable.wind_6, R.drawable.wind_7, R.drawable.wind_8, R.drawable.wind_9,};
+    private Integer[] mMeditation = {R.drawable.meditation_1, R.drawable.meditation_2, R.drawable.meditation_3, R.drawable.meditation_4, R.drawable.meditation_4, R.drawable.meditation_5, R.drawable.meditation_6, R.drawable.meditation_7, R.drawable.meditation_8, R.drawable.meditation_9,};
+
     private ArrayList<String> mMusicList = new ArrayList();
+    private String name = "雨声-1";
+    private Random random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         initPickerView();
         setListener();
+        updateImg();
         myRegisterReceiver();
     }
 
@@ -83,19 +99,18 @@ public class MainActivity extends AppCompatActivity {
         //optionsPicker.setHeadBackgroundColor(Color.RED);//设置头部背景色
         // 设置数据
         mMusicList = new ArrayList<>(Arrays.asList(mMusicName));
+
         mMusicPik.setPicker(mMusicList);
         // 设置选项单位
 //        mOptionsPickerView.setLabels("性");
         mMusicPik.setOnOptionsSelectListener(new OptionsPickerView.OnOptionsSelectListener() {
             @Override
             public void onOptionsSelect(int option1, int option2, int option3) {
-                String sex = mMusicList.get(option1);
-                tvMainMusicType.setText(sex);
+                name = mMusicList.get(option1);
+                tvMainMusicType.setText(name);
                 Intent intent;
-
-
                 if (isStart) {
-                    intent = new Intent("cn.xunlebao.voice.music");
+                    intent = new Intent(PlayMusicService.MESSAGE_ACTION_MUSIC);
                     intent.putExtra("music", mMusicRec[option1]);
                     sendBroadcast(intent);
                 } else {
@@ -108,6 +123,93 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+        mTimePicker = new OptionsPickerView(this);
+        mTimePicker.setCancelable(true);//是否点击外部消失
+        mTimePicker.setTextSize(16f);//设置滚轮字体大小
+        mTimePicker.setTitle("倒计时(分)");//设置标题
+        mTimePicker.setCancelText("取消");//设置取消文字
+        mTimePicker.setCancelTextColor(Color.parseColor("#39B54A"));//设置取消文字颜色
+        mTimePicker.setCancelTextSize(14f);//设置取消文字大小
+        mTimePicker.setSubmitText("确认");//设置确认字体
+        mTimePicker.setCancelTextSize(14f);//设置去人字体大小
+        mTimePicker.setSubmitTextColor(Color.parseColor("#39B54A"));//设置确认字体颜色
+        //optionsPicker.setHeadBackgroundColor(Color.RED);//设置头部背景色
+        // 设置数据
+        final ArrayList<String> times = new ArrayList<>();
+        times.add("关闭");
+        times.add("15");
+        times.add("30");
+        times.add("60");
+        times.add("90");
+        times.add("120");
+        mTimePicker.setPicker(times);
+        // 设置选项单位
+//        mOptionsPickerView.setLabels("性");
+        mTimePicker.setOnOptionsSelectListener(new OptionsPickerView.OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int option1, int option2, int option3) {
+                String time = times.get(option1);
+                tvMainSleepTime.setText(time);
+                if ("关闭".equals(time)) {
+                    Toast.makeText(MainActivity.this, time, Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, time, Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+    }
+
+
+    private void updateImg() {
+        random = new Random();
+        mThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                try {
+                    while (true) {
+                        final int rec;
+                        Thread.sleep(10000);
+                        switch (name.substring(0, 1)) {
+                            case "雨":
+                                rec = mRain[random.nextInt(mRain.length)];
+                                break;
+                            case "火":
+                                rec = mFire[random.nextInt(mFire.length)];
+                                break;
+                            case "森":
+                                rec = mForest[random.nextInt(mForest.length)];
+                                break;
+                            case "海":
+                                rec = mSea[random.nextInt(mSea.length)];
+                                break;
+                            case "风":
+                                rec = mWind[random.nextInt(mWind.length)];
+                                break;
+                            case "冥":
+                                rec = mMeditation[random.nextInt(mMeditation.length)];
+                                break;
+                            default:
+                                rec = R.drawable.forest_5;
+
+                        }
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                activityMain.setBackgroundResource(rec);
+                            }
+                        });
+                    }
+
+                } catch (InterruptedException e) {
+
+
+                }
+            }
+        });
+        mThread.start();
     }
 
     private void setListener() {
@@ -132,19 +234,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-    }
-
-    /*
-         * 判断服务是否启动,context上下文对象 ，className服务的name
-         */
-    private boolean isServiceRunning() {
-        ActivityManager manager = (ActivityManager) getSystemService(ACTIVITY_SERVICE);
-        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
-            if ("com.xunlebao.voice.PlayMusicService".equals(service.service.getClassName())) {
-                return true;
-            }
-        }
-        return false;
     }
 
     /**
@@ -184,6 +273,7 @@ public class MainActivity extends AppCompatActivity {
     public void onViewClicked(View view) {
         switch (view.getId()) {
             case R.id.tv_main_sleep_time:
+                mTimePicker.show();
                 break;
             case R.id.tv_main_music_type:
                 mMusicPik.show();
--- app/src/main/java/cn/xunlebao/voice/PlayMusicService.java
+++ app/src/main/java/cn/xunlebao/voice/PlayMusicService.java
@@ -9,6 +9,7 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Binder;
 import android.os.IBinder;
+import android.util.Log;
 
 import java.io.IOException;
 
@@ -16,10 +17,10 @@ import java.io.IOException;
  * Created by Vincent on 2017/7/18.
  */
 
-public class PlayMusicService extends Service implements MediaPlayer.OnCompletionListener {
-    public static final String MESSAGE_ACTION = "cn.xunlebao.voice.music";
-    MediaPlayer player;
-    private int mMusicId;
+public class PlayMusicService extends Service {
+    public static final String MESSAGE_ACTION_MUSIC = "cn.xunlebao.voice.MUSIC_MP3";
+    static MediaPlayer player;
+    private static int mMusicId;
     private final IBinder binder = new AudioBinder();
     private MusicReceiver mReceiver;
     private IntentFilter mFilter;
@@ -29,26 +30,26 @@ public class PlayMusicService extends Service implements MediaPlayer.OnCompletio
         // TODO Auto-generated method stub
         return binder;
     }
-
-    /**
-     * 当Audio播放完的时候触发该动作
-     */
-    @Override
-    public void onCompletion(MediaPlayer player) {
-        // TODO Auto-generated method stub
-//        stopSelf();//结束了，则结束Service
-        if (!player.isPlaying()) {
-            player.start();
-        }
-    }
+//
+//    /**
+//     * 当Audio播放完的时候触发该动作
+//     */
+//    @Override
+//    public void onCompletion(MediaPlayer player) {
+//        // TODO Auto-generated method stub
+////        stopSelf();//结束了，则结束Service
+//        if (!player.isPlaying()) {
+//            player.start();
+//        }
+//    }
 
     //在这里我们需要实例化MediaPlayer对象
     public void onCreate() {
         super.onCreate();
-//        mReceiver = new MusicReceiver();
-//        mFilter = new IntentFilter();
-//        mFilter.addAction(MESSAGE_ACTION);
-//        registerReceiver(mReceiver, mFilter);
+        mReceiver = new MusicReceiver();
+        mFilter = new IntentFilter();
+        mFilter.addAction(MESSAGE_ACTION_MUSIC);
+        registerReceiver(mReceiver, mFilter);
 
     }
 
@@ -65,10 +66,12 @@ public class PlayMusicService extends Service implements MediaPlayer.OnCompletio
         mMusicId = intent.getIntExtra("music", 0);
         //我们从raw文件夹中获取一个应用自带的mp3文件
         player = MediaPlayer.create(this, mMusicId);
-        player.setOnCompletionListener(this);
+        player.setLooping(true);
+//        player.setOnCompletionListener(this);
         if (!player.isPlaying()) {
             player.start();
         }
+
         return START_STICKY;
     }
 
@@ -91,21 +94,18 @@ public class PlayMusicService extends Service implements MediaPlayer.OnCompletio
 
     }
 
-  public  class MusicReceiver extends BroadcastReceiver {
+    public static class MusicReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
             mMusicId = intent.getIntExtra("music", -1);
             if (mMusicId != -1 && player != null) {
-                try {
-                    if (player.isPlaying()){
-                        player.stop();
-                        player.release();
-                    }
-                    player.setDataSource(context, Uri.parse("android.resource://" + context.getPackageName() + "/" + mMusicId));
-                    player.start();
-                } catch (IOException e) {
-                    e.printStackTrace();
+                if (player.isPlaying()) {
+                    player.stop();
+                    player.release();
                 }
+                player = MediaPlayer.create(context, mMusicId);
+                player.setLooping(true);
+                player.start();
             }
         }
     }
